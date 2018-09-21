68011c2c3e49bd18a974dc6c2c776191ac1ca2c6
Andre Rozendaal <13313480+AndreRozendaal@users.noreply.github.com>
2018-09-21 11:27:15 +0200

    Update cert.ps1

--- cert.ps1
+++ cert.ps1
@@ -2,13 +2,14 @@ $VerbosePreference = "Continue"
 $DebugPreference = "Continue"
 
 Param(
-    [parameter(Mandatory=$true)][String]$resourceGroupName,
+    [parameter(Mandatory=$true)][String]$ResourceGroupName,
     [parameter(Mandatory=$true)][String]$ApplicationGateway,
     [parameter(Mandatory=$true)][String]$Password,
+    [parameter(Mandatory=$true)][String]$WorkingDirectory
 )
 
-$hostn=(Get-AzureRmPublicIpAddress -ResourceGroupName $(ResourceGroupName) -Name "pip-$(ApplicationGateway)").DnsSettings.Fqdn
+$hostn=(Get-AzureRmPublicIpAddress -ResourceGroupName $ResourceGroupName -Name "pip-$ApplicationGateway").DnsSettings.Fqdn
 write-host "##vso[task.setvariable variable=host]$hostn"
 $cert=New-SelfSignedCertificate -DnsName $hostn -CertStoreLocation "cert:\CurrentUser\My"
-$pwd = ConvertTo-SecureString -String $(Password) -Force -AsPlainText
-Export-PfxCertificate -cert "cert:\CurrentUser\My\$cert.thumbprint" -FilePath "$(System.DefaultWorkingDirectory)\appgwcert.pfx" -Password $pwd -Force
+$pwd = ConvertTo-SecureString -String $Password -Force -AsPlainText
+Export-PfxCertificate -cert "cert:\CurrentUser\My\$cert.thumbprint" -FilePath "$WorkingDirectory\appgwcert.pfx" -Password $pwd -Force
