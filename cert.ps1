a465da71eda4bdfae6ce62f719727912adb9047c
Andre Rozendaal <13313480+AndreRozendaal@users.noreply.github.com>
2018-09-21 12:38:44 +0200

    Update cert.ps1

--- cert.ps1
+++ cert.ps1
@@ -16,6 +16,9 @@ write-verbose "End New-SelfSignedCertificate"
 write-verbose "Password: $Password : $pwd"
 $pwd = ConvertTo-SecureString -String $Password -Force -AsPlainText
 
+write-verbose "dir cert cert:\CurrentUser\My"
+dir cert:\CurrentUser\My
+
 write-verbose "Start Export-PfxCertificate"
 Export-PfxCertificate -cert "cert:\CurrentUser\My\$cert.thumbprint" -FilePath "$WorkingDirectory\appgwcert.pfx" -Password $pwd -Force
 write-verbose "End Export-PfxCertificate"
